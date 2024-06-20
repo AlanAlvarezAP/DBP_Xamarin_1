2fb97e6594d90b3fa68166dcc977aa6ddb84452e
AlanAlvarezAP <144193511+AlanAlvarezAP@users.noreply.github.com>
2024-06-20 00:57:41 -0500

    Update MainPage.xaml.cs

--- App4/App4/MainPage.xaml.cs
+++ App4/App4/MainPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace App4
